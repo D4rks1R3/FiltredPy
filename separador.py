83d80e973f15c1e234fe3c3973561d6411b10f6b
c4l4ng0_m4t4d0r <40896649+D4rks1R3@users.noreply.github.com>
2019-04-10 09:28:38 -0300

    Update separador.py

--- separador.py
+++ separador.py
@@ -1,67 +1,78 @@
-#!/usr/bin/python3.6
-import re
+# -*- coding: utf-8 -*-
+#!/usr/bin/python3
+
+import glob
 import requests
 import time
 import os
-tex = 'tst.txt'
+import platform
+
 
 def down():
-   print('Inicio  >>', time.strftime('%H:%M:%S')+'\n')
-   url = "http://files.cedrotech.com/tickbytick/Bmf/20190109.log"
-   r = requests.get('http://127.0.0.1/logs.log')
-   t = str(r.text)
-   tex = 'tst.txt'
-   whi = open(tex,"a+")
-   whi.write(t)
-   whi.close()
-   print("COMPLETO!")
-   print("FIM >>", time.strftime('%H:%M:%S')+'\n')
+  print('Inicio  >>', time.strftime('%H:%M:%S')+'\n')
+  url = "http://files.cedrotech.com/tickbytick/Bmf/20190109.log"
+  r = requests.get('http://127.0.0.1/logs.log')
+  t = str(r.text)
+  tex = 'tst.txt'
+  whi = open(tex,"a+")
+  whi.write(t)
+  whi.close()
+  print("COMPLETO!")
+  print("FIM >>", time.strftime('%H:%M:%S')+'\n')
+
+def comando():
+    if platform.system() ==  "Windows":
+          print("Filtragem terminada com sucesso")
+          
+    if platform.system() == "Linux":
+          print("Filtragem terminada com sucesso")
+          
+          
+
+arqu = str(input("Nome para Filtragem do primeiro: ")).upper()
+arqd = str(input("Nome para Filtragem do segundo arquivo: ")).upper()
 
 def separador():
    try:
-       com = str(input("arquivo .log: "))   
-       file = open(com, "r+")
-       ler = file.readlines()
-       for i in ler:
-         a = i.split("|", 4)
-         p, b, o, m  = a[0:4]
-         tt = b.replace("-", ".")
-         t, k = a[0:2]
-         tr = t[0:3]
-                  
-         total = p[0:4]+","+tt+',,,'+o+','+m
-        
-         if tr == 'WIN':
+      
+      li = glob.glob("*.log")
+      for i in li:
+         print("lendo arquivo -->", i)
+         time.sleep(1)
+         ler = open(i, 'r+')
+         lend = ler.readlines()
+         for dd in lend:                
+            a = dd.split("|", 4)
+            p, b, o, m  = a[0:4]
+            tt = b.replace("-", ".")
+            t, k = a[0:2]
+            tr = t[0:4]
+            total = p[0:4]+","+tt+',,,'+o+','+m
+
+            if arqu == " ":
+               print("Preencha o primeiro campo!")
+            if arqd == " ":
+               print("Preencha o segundo campo")            
+          
+            if tr == arqu:
                print(total)
-               fi = open("win.txt", "a+")
+               fi = open(arqu+".txt", "a+")
                fi.writelines(str(total)+'\n')
                fi.close()
          
-         if tr == 'WDO':
+            if tr == arqd:
                print(total)          
-               fj = open("wdo.txt", "a+")
+               fj = open(arqd+".txt", "a+")
                fj.writelines(str(total)+'\n')
                fj.close()
-
-         file.close()
-        
+            
+            
+      ler.close()
+      comando()
+            
    except Exception as e:
-         print("Erro encontrado!")
-         os.system("pause")
+      print('erro:', e)
+    
+  
 
 separador()
-
-"""
-def organizar_alfabeticamente(lista):
-   "Organiza alfabeticamente as strings contidas dentro de uma lista."
-   for x in range (len(lista)):
-      
-      for y in range (len(lista)):
-         
-         if lista[x] < lista[y]:
-            lista[x], lista[y] = lista[y], lista[x]
-lista = ["h", "g", "g",  "f"]
-print ("Antes:", lista)
-organizar_alfabeticamente(lista)
-print ("Depois:", lista)
-"""
